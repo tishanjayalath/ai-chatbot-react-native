1e3949846737e9420000eee0671d911e094f945d
tishanjayalath <tishanjayalath18@gmail.com>
2024-02-22 22:08:21 +0530

    my ai chat bot app

--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL='http://192.168.0.113:3000/api/bardapi' //Replace with System PC IP address
+const BASE_URL='http://192.168.1.22:3000/api/bardapi' //Replace with System PC IP address
 
 const getBardApi=(userMsg)=>axios.get(BASE_URL+"?ques="+userMsg);
 
